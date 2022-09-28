8e27d2daddcdc3e9f76687547d1a5e9dcdde3766
Wilhelm Ågren <wilhelmagren98@gmail.com>
2022-09-28 22:13:25 +0200

    Cargo files, Arrow2 dependency.

--- lib/impl/src/lib.rs
+++ lib/impl/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate libc;
+use arrow2::ffi::ArrowSchema;
 
 #[no_mangle]
-pub extern "C" fn callwithtable(data: &mut [ArrowSchema* schema])  {
+pub extern "C" fn callwithtable(data: &mut ArrowSchema)  {
+    println!("Hello from Rust, with ArrowSchema!");
 }
