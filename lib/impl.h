6bb10041521e595814316b37f8aec8380a9754aa
Rickard Lundin <rickard@x14.se>
2022-09-29 01:40:11 +0200

    further on almost compiling

--- lib/impl.h
+++ lib/impl.h
@@ -1,3 +1,5 @@
 // NOTE: You could use https://michael-f-bryan.github.io/rust-ffi-guide/cbindgen.html to generate
+#include <stdint.h>
+#include <stdlib.h>
 #include "../api/ARROW_C_DATA_INTERFACE.h"
 void callwithtable(ArrowSchema *schema);
