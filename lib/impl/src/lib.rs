79e27cd94fc00b1555904c9f8eedbb36d885f9b3
Wilhelm Ågren <wilhelmagren98@gmail.com>
2022-09-29 20:26:35 +0200

    Rebase back to Apache Arrow crate, but v9.0.0

--- lib/impl/src/lib.rs
+++ lib/impl/src/lib.rs
@@ -1,7 +1,8 @@
 extern crate libc;
-use arrow2::ffi::ArrowSchema;
+use arrow::ffi::FFI_ArrowSchema;
 
 #[no_mangle]
-pub extern "C" fn callwithtable(data: &mut ArrowSchema)  {
-    println!("Hello from Rust, with ArrowSchema: {:?}", data);
+pub extern "C" fn callwithtable(schema: &mut FFI_ArrowSchema)  {
+    println!("Hello from Rust, with FFI_ArrowSchema: {:?}", schema);
 }
+
