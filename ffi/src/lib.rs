6f72b88e4853c4fe8a23577c256e61de7fae7cec
Eli Vance <wilhelmagren98@gmail.com>
2022-10-06 18:18:53 +0200

    No more arrow2, everything worksgit add .! YAY

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,33 +1,10 @@
 use libc::c_int;
-use arrow2::datatypes::Field;
-use arrow2::array::Array;
-use arrow2::ffi::
-{
-    ArrowArray,
-    ArrowSchema,
-    import_array_from_c,
-    import_field_from_c
-};
+use arrow::ffi::{FFI_ArrowArray, FFI_ArrowSchema};
+use arrow::array::{Array, ArrayRef, make_array_from_raw};
 
-// https://github.com/jorgecarleitao/arrow2/blob/main/examples/ffi.rs
 #[no_mangle]
-pub extern "C" fn call_with_ffi(array: ArrowArray, schema: &ArrowSchema) -> c_int {
-    let field: Field = match unsafe
-    { import_field_from_c(schema) } {
-        Ok(f) => f,
-        Err(e) => panic!("Could not import_field_from_c, {:?}", e),
-    };
-
-    println!("Imported field from schema: {:?}", field);
-    
-    let _arr: Box<dyn Array> = match unsafe
-    { import_array_from_c(array, field.data_type) } {
-        Ok(a) => a,
-        Err(e) => panic!("Could not import_array_from_c, {:?}", e),
-    };
-
-    println!("Hello from Rust, with Arrow Array");
-
+pub extern "C" fn call_with_ffi(ffi_array: *const FFI_ArrowArray, ffi_schema: *const FFI_ArrowSchema) -> c_int {
+    let _arr = unsafe { make_array_from_raw(ffi_array, ffi_schema); };
     1 as c_int
 }
 
