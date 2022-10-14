7305e0c79c1fee8cc0cda53d1f5b4f9583987a91
Wilhelm Ågren <wilhelmagren98@gmail.com>
2022-10-14 21:22:30 +0200

    (#36) using custom export with schema but still kernel panic...

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,11 +1,12 @@
 use libc::c_int;
 use arrow::ffi::{FFI_ArrowArray, FFI_ArrowSchema};
-use arrow::array::{Array, ArrayRef, make_array_from_raw};
+use arrow::array::{Array, make_array_from_raw};
+use arrow::datatypes::{DataType, Schema, Field};
 
 #[no_mangle]
 pub extern "C" fn call_with_ffi(
-    ffi_array: *const FFI_ArrowArray,
-    ffi_schema: *const FFI_ArrowSchema
+    ffi_array: &FFI_ArrowArray,
+    ffi_schema: &FFI_ArrowSchema
  ) -> c_int {
 
     let _arr = unsafe { make_array_from_raw(ffi_array, ffi_schema); };
@@ -14,9 +15,14 @@ pub extern "C" fn call_with_ffi(
 
 #[no_mangle]
 pub extern "C" fn call_with_ffi_schema(
-    ffi_schema: *const FFI_ArrowSchema
+    ffi_schema: &FFI_ArrowSchema
 ) -> c_int {
+    
+    let schema: Schema = match Schema::try_from(ffi_schema) {
+        Ok(s) => s,
+        Err(e) => panic!("Could not convert to Rust schema: {:?}", e)
+    };
 
-    println!("Hello from Rust! Here is address to schema: {:?}", ffi_schema); 
+    println!("Hello from Rust! Here is the schema: {:?}", schema); 
     1 as c_int
 }
