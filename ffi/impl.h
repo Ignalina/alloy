c676edb0c9f411bbc649689214a9abc2fb0f60a6
Wilhelm Ågren <wilhelmagren98@gmail.com>
2022-10-16 10:58:45 +0200

    Remove some unnecessary includes from C header file.

--- ffi/impl.h
+++ ffi/impl.h
@@ -1,9 +1,4 @@
-#include <stdarg.h>
-#include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
-
-
 #include "../cdata/arrow/c/abi.h"
 
 int  from_chunks_ffi(const ArrowArray *arrptr, const ArrowSchema *schptr, uintptr_t l);
