fe6e1590bb1a290526355d1daa60e9f31c4f85f8
wilhelmagren <wilhelmagren98@gmail.com>
2023-11-11 15:50:31 +0100

    fix(rust): cargo format and check

--- alloy.rs/src/lib.rs
+++ alloy.rs/src/lib.rs
@@ -1,18 +1,18 @@
 /*
 * MIT License
-* 
+*
 * Copyright (c) 2023 Rickard Ernst Björn Lundin & Wilhelm Ågren
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,16 +20,15 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
-* 
+*
 * File created: 2023-11-11
 * Last updated: 2023-11-11
 */
 
 pub mod logging;
 
-use log::{trace, debug, info, warn, error};
+use log::{debug, error, info, trace, warn};
 use std::ffi::CStr;
-
 
 #[no_mangle]
 pub extern "C" fn init_rs_logging() {
@@ -37,9 +36,7 @@ pub extern "C" fn init_rs_logging() {
 }
 
 #[no_mangle]
-pub extern "C" fn test_rs_logging(
-    message: *const libc::c_char,
-) {
+pub extern "C" fn test_rs_logging(message: *const libc::c_char) {
     let cstr = unsafe { CStr::from_ptr(message) };
     let string = cstr.to_str().unwrap();
 
@@ -50,12 +47,11 @@ pub extern "C" fn test_rs_logging(
     error!("{}", &string);
 }
 
-
 #[cfg(test)]
 pub mod tests {
 
-    use std::ffi::CString;
     use super::*;
+    use std::ffi::CString;
 
     #[test]
     fn test_init_rs_logging() {
--- alloy.rs/src/logging.rs
+++ alloy.rs/src/logging.rs
@@ -1,18 +1,18 @@
 /*
 * MIT License
-* 
+*
 * Copyright (c) 2023 Rickard Ernst Björn Lundin & Wilhelm Ågren
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,11 +20,10 @@
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
-* 
+*
 * File created: 2023-11-11
 * Last updated: 2023-11-11
 */
-
 
 use chrono::Local;
 use env_logger::Builder;
@@ -33,31 +32,27 @@ use log::LevelFilter;
 use std::env;
 use std::io::Write;
 
-
 fn get_log_level_from_env() -> LevelFilter {
     match env::var("RUST_LOG") {
-        Ok(val) => {
-            match val.as_str() {
-                "OFF" => LevelFilter::Off,
-                "TRACE" => LevelFilter::Trace,
-                "INFO" => LevelFilter::Info,
-                "WARN" => LevelFilter::Warn,
-                "WARNING" => LevelFilter::Warn,
-                "ERR" => LevelFilter::Error,
-                "ERROR" => LevelFilter::Error,
-                &_ => LevelFilter::Warn,
-            }
+        Ok(val) => match val.as_str() {
+            "OFF" => LevelFilter::Off,
+            "TRACE" => LevelFilter::Trace,
+            "INFO" => LevelFilter::Info,
+            "WARN" => LevelFilter::Warn,
+            "WARNING" => LevelFilter::Warn,
+            "ERR" => LevelFilter::Error,
+            "ERROR" => LevelFilter::Error,
+            &_ => LevelFilter::Warn,
         },
         Err(_) => LevelFilter::Warn,
     }
 }
 
 pub fn init() {
-
     let log_level: LevelFilter = get_log_level_from_env();
 
     Builder::new()
-        .format(| buf, record | {
+        .format(|buf, record| {
             writeln!(
                 buf,
                 "{} [ {} ] - {}",
@@ -69,4 +64,3 @@ pub fn init() {
         .filter(None, log_level)
         .init();
 }
-
